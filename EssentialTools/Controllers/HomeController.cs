f489f3d59a73d9e8ce3923231c0b36e7988dcb0c
tonousa <tonousa@hotmail.com>
2015-07-02 16:27:32 -0400

    Commit proj

---
+++ EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+using Ninject;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Product[] products = {
+            new Product {Name = "Kayak", Category = "Watersports", Price=275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price=49.95M},
+            new Product {Name = "Soccer Ball", Category = "Soccer", Price=9.9M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price=34.95M}
+        };
+        private IValueCalculator calc;
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public ActionResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+
+            decimal totalValue = cart.CalculateProductTotal();
+
+            return View(totalValue);
+        }
+    }
+}
---
+++ EssentialTools/Global.asax.cs
@@ -0,0 +1,22 @@
+﻿using EssentialTools.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EssentialTools
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            DependencyResolver.SetResolver(new NinjectDependencyResolver());
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
